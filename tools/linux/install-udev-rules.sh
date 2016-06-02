79b2e81fa31ad5017e24604de4e42526a4534376
Ekawahyu Susilo <ekawahyu@aisenke.com>
2016-06-02 02:05:37 -0500

    added dfu-util static built from https://github.com/arduino/dfu-utils-cross, work in progress for dfu_upload.bat

--- tools/linux/install-udev-rules.sh
+++ tools/linux/install-udev-rules.sh
@@ -5,6 +5,10 @@ if sudo [ -w /etc/udev/rules.d ]; then
     sudo cp -v 45-maple.rules /etc/udev/rules.d/45-maple.rules
     sudo chown root:root /etc/udev/rules.d/45-maple.rules
     sudo chmod 644 /etc/udev/rules.d/45-maple.rules
+    echo "Copying STM32-DFU udev rules..."
+    sudo cp -v 90-stm32-dfu.rules /etc/udev/rules.d/90-stm32-dfu.rules
+    sudo chown root:root /etc/udev/rules.d/90-stm32-dfu.rules
+    sudo chmod 644 /etc/udev/rules.d/90-stm32-dfu.rules
 else
     echo "Couldn't copy to /etc/udev/rules.d/; you probably have to run this script as root? Or your distribution of Linux doesn't include udev; try running the IDE itself as root."
 fi
